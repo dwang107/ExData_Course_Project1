77a6ff110a1758111a06ab9dbd74ed789ff9fade
dwang107 <72616259+dwang107@users.noreply.github.com>
2022-03-22 23:33:38 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,9 @@
 ## plot 3
+raw <- read.csv("./data/household_power_consumption.txt", header = TRUE, sep = ";")
+library(lubridate)
+## Only data from 2007-02-01 and 2007-02-02 will be used
+dataFeb2007 <- subset(raw, as.Date(raw$Date, "%d/%m/%Y") <= as.Date("2007-02-02", "%Y-%m-%d") 
+                      & as.Date(raw$Date, "%d/%m/%Y") >= as.Date("2007-02-01", "%Y-%m-%d"))
 par(mfrow = c(1,1))
 plot3data <- data.frame(DateTime = dmy_hms(paste(dataFeb2007$Date, dataFeb2007$Time), tz = "UTC"),
                         sub_meeting1 = as.numeric(dataFeb2007$Sub_metering_1),
