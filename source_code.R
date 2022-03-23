c1211c1be4b293e3efdf12318057e1c8cc1156e9
dwang107 <72616259+dwang107@users.noreply.github.com>
2022-03-22 23:28:33 -0500

    Update source_code.R

--- source_code.R
+++ source_code.R
@@ -34,8 +34,11 @@ plot(plot3data$DateTime, plot3data$sub_meeting1, type = "l", col = "black", xlab
 lines(plot3data$DateTime, plot3data$sub_meeting2, col = "red")
 lines(plot3data$DateTime, plot3data$sub_meeting3, col = "blue")
 legend("topright",
+       inset = c(0.05,0),
+       cex = 0.7,
        lwd = c(2,2,2),
        col = c("black", "red", "blue"), 
+       bty = "n",
        legend = c("Sub_meeting_1", "Sub_meeting_2", "Sub_meeting_3"))
 dev.copy(png, file = "plot3.png")  
 dev.off()  
